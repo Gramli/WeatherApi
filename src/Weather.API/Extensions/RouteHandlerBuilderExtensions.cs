145bcc90e7a5ef42add0c8b1a6785a09492569b1
Gramli <Gramli@seznam.cz>
2025-12-22 13:28:35 +0100

    remove unused

--- src/Weather.API/Extensions/RouteHandlerBuilderExtensions.cs
+++ src/Weather.API/Extensions/RouteHandlerBuilderExtensions.cs
@@ -6,12 +6,6 @@ namespace Weather.API.Extensions
     {
         public static RouteHandlerBuilder ProducesDataResponse<TResponse>(
             this RouteHandlerBuilder builder,
-            int statusCode = StatusCodes.Status200OK,
-            params string[] additionalContentTypes)
-            => builder.Produces<DataResponse<TResponse>>(statusCode, null, additionalContentTypes);
-
-        public static RouteHandlerBuilder ProducesDataResponse<TResponse>(
-            this RouteHandlerBuilder builder,
             params string[] additionalContentTypes)
             => builder.Produces<DataResponse<TResponse>>(StatusCodes.Status200OK, null, additionalContentTypes);
     }
